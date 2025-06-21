2002cb698049ce22d4b45d1888e64233440c1059
kalingasajja <154085424+kalingasajja@users.noreply.github.com>
2025-06-21 22:08:23 +0530

    Update demo.py

--- demo.py
+++ demo.py
@@ -27,7 +27,7 @@ def create_mask(pred_mask):
 @st.cache_resource
 def load_model():
     return tf.keras.models.load_model(
-        "Main_model_2.keras", custom_objects={"dice_coef": dice_coef}
+        "Main_model_2.keras"
     )
 
 model = load_model()
